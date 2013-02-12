b30c7d9a3be3927265ada8da408e7c27df2db93b
Azeem Ahmed <azeem498@yahoo.co.in>
2013-02-12 18:06:04 +0530

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,21 +2,46 @@ require_relative '../spec_helper'
 
 describe "Static pages" do
 	
-describe "Home page" do
+	describe "Home page" do
 	
-it "should have the content 'Chatter App'" do
-visit '/static_pages/home'
-page.should have content('Chatter App')
+		it "should have the h1 'Chatter App'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Chatter App')
+		end
+		
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('title',
+							:text => "Chatter Application | Home")
+		end
+	end					
 
-end
-end
-
-describe "Help page" do
+	describe "Help page" do
 	
-it "should have the content 'Help'" do
-visit '/static_pages/help'
-page.should have content('Help')
+		it "should have the h1 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		end
+		
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('title',
+							:text => "Chatter Application | Help")
+		end
+	end
 
-end
-end
+	describe "About page" do
+	
+		it "should have the h1 'About Chatter'" do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About Chatter')
+		end
+		
+		it "should have the title 'About Chatter'" do
+			visit '/static_pages/about'
+			page.should have_selector('title',
+							:text => "Chatter Application | About")
+		end					
+	end
+
 end
